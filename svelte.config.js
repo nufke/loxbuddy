008cb327461d1e0549f4ac8d5162ef31ef3b33b6
nufke <nufke@barnasconi.net>
2025-06-11 19:58:58 +0200

    suppress warning a11y_no_static_element_interactions

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,14 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({ out: 'build' })
+	},
+	 compilerOptions: {
+			warningFilter: (warning) => {
+				const ignore = [
+					'a11y_no_static_element_interactions'
+				]
+			return !ignore.includes(warning.code)
+		}
 	}
 };
 
