67cef1a42c73ff97390d90b5ef9e3b44ed93deeb
nufke <nufke@barnasconi.net>
2025-07-12 13:35:50 +0200

    icon fallback

--- src/lib/components/lb-icon-by-name.svelte
+++ src/lib/components/lb-icon-by-name.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import * as icons from '@lucide/svelte';
 	import { inlineSvg } from '@svelte-put/inline-svg';
-
+	import { Image } from '@lucide/svelte';
 	export let name;
 </script>
 
-{#if icons[name]}
-<svelte:component this={icons[name]} {...$$props} />
+{#if name == '/loxicons/'} <!-- no icon name given -->
+	<Image/> 
+{:else if icons[name]}
+	<svelte:component this={icons[name]} {...$$props} />
 {:else}
-<svg use:inlineSvg={name} {...$$props}></svg>
+	<svg use:inlineSvg={name} {...$$props}></svg>
 {/if}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { inlineSvg } from '@svelte-put/inline-svg/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [
+		tailwindcss(),
+		sveltekit()
+	]
 });
