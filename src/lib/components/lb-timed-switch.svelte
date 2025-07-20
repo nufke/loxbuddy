b0bce55083d3d29bbe520559c39b74e011a892f5
nufke <nufke@barnasconi.net>
2025-07-20 18:50:13 +0200

    update timed-switch

--- src/lib/components/lb-timed-switch.svelte
+++ src/lib/components/lb-timed-switch.svelte
@@ -10,36 +10,79 @@
 
 	let { control, controlOptions = DEFAULT_CONTROLOPTIONS }: { control: Control, controlOptions: ControlOptions } = $props();
 
+	/*
+		deactivationDelay:
+		 0: output turned off
+		-1: output permanently on
+		other: count down from deactivationDelayTotal
+	*/
 	let deactivationDelay = $derived(Number(store.getState(control.states.deactivationDelay)) || 0);
 
-	let modalButtons: SingleButtonView[] = $derived([
+	let modalButton1: SingleButtonView = $derived(
 		{
-			name: (deactivationDelay == 0) ? $_('Turn on') : $_('Turn off'),
+			name: (deactivationDelay == 0) ? $_('Turn on') : ((deactivationDelay == -1) ? $_('Turn off') : $_('Switch on permanently')),
 			type: 'button',
 			color: '',
 			click: (e: any) => {
-				publishTopic(control.uuidAction, (deactivationDelay == 0) ? 'on' : 'off');
+				publishTopic(control.uuidAction, (deactivationDelay == 0) ? 'on' : ( (deactivationDelay == -1) ? 'off' : 'on' ));
 			}
-		},
+		}
+	);
+
+	let modalButton2: SingleButtonView = $derived(
 		{
-			name: (deactivationDelay > 0) ? $_('Stop timer') : $_('Start timer'),
+			name: (deactivationDelay == 0) ? $_('Start timer') : $_('Turn off'),
 			type: 'button',
 			color: '',
 			click: () => {
-				publishTopic(control.uuidAction, (deactivationDelay > 0) ? 'off' : 'pulse');
+				publishTopic(control.uuidAction, (deactivationDelay == 0) ? 'pulse' : 'off');
 			}
 		}
-	]);
+	);
+
+	let modalButton3: SingleButtonView = $derived(
+		{
+			name: $_('Restart timer'),
+			type: 'button',
+			color: '',
+			click: () => {
+				publishTopic(control.uuidAction, 'pulse');
+			}
+		}
+	);
+
+	function formatTime(delay: number) {
+		if (delay > 0) {
+			let minutes = Math.floor(delay/60);
+			let seconds = delay - minutes * 60;
+			let time = (minutes > 0) ? String(minutes) + ' min ' : '';
+			time += String(seconds) + ' s';
+			return time;
+		}
+		if (delay == -1) {
+			return $_('On');
+		}
+	}
+
+	function setButtons() {
+		let modalButtons: SingleButtonView[] = [];
+		modalButtons.push(modalButton1);
+		if (deactivationDelay != -1) modalButtons.push(modalButton2);
+		if (deactivationDelay > 0) modalButtons.push(modalButton3);
+		return modalButtons;
+	}
 
 	let controlButtons: SingleButtonView[] = $state([
 		{
 			iconName: '/loxicons/IconsFilled/timer-2.svg',
 			type: 'button',
 			iconColor: 'dark:fill-surface-50 fill-surface-950',
-			click: () => publishTopic(control.uuidAction, (deactivationDelay == 0) ? 'pulse' : 'off')
+			click: () => {
+				publishTopic(control.uuidAction, 'pulse')
+			}
 		}
 	]);
-	
+
 	let modal: ModalView = $state({
 		action: (state: boolean) => {modal.state = state},
 		state: controlOptions.showModal
@@ -50,11 +93,14 @@
 		control: control,
 		isFavorite: controlOptions.isFavorite,
 		iconName: store.getIcon(control, controlOptions.isSubControl),
+		iconColor: (deactivationDelay == -1 || deactivationDelay > 0) ? 'dark:fill-primary-500 fill-primary-700' : 'fill-surface-950 dark:fill-surface-50',
 		textName: control.name,
+		statusName: formatTime(deactivationDelay),
+		statusColor: (deactivationDelay == -1 || deactivationDelay > 0) ? 'dark:text-primary-500 text-primary-700' : '',
 		buttons: controlButtons,
 		modal: {
 			...modal,
-			buttons: modalButtons
+			buttons: setButtons()
 		}
 	});
 </script>
